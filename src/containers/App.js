eadcba6ac714a000caf3787ca9175bff76cc0efc
alvarojossue <alvaro.castillo23@gmail.com>
2022-08-25 19:10:53 -0400

    added scrolling to cardList and reorganized folders

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import { players } from '../components/players'; 
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      players: players,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+  }
+
+  render() {
+    const filteredPlayers = this.state.players.filter(player => {
+      return player.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+      return (
+      <div>
+        <div className='tc' style={{border: '1px solid white'}}>
+          <h1 className='f1 washed-blue'>Real Friends</h1>
+          <p className='f4 washed-blue avenir'>Real Madrid Squad 2022-2023</p>
+          <SearchBox searchChange={this.onSearchChange} />
+        </div>
+        <div className='tc'>
+          <Scroll>
+            <CardList players={filteredPlayers}/>
+          </Scroll>
+        </div>
+        <div className='tc washed-blue avenir '>
+          <p>Made with 14 Champions League Trophies</p>
+        </div>
+      </div>
+      );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Hello from './Hello';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
  
